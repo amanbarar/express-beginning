56508c4d217fa0ab7c5ba47514c7235812a5b7f9
Aman Barar <barar.amanjeet.aman@gmail.com>
2023-03-26 16:25:18 +0530

    pug templating

--- index.js
+++ index.js
@@ -3,19 +3,11 @@ var app = express();
 
 var things = require('./things.js');
 
-app.use('/things', function(req, res, next){
-   console.log("A request for things received at " + Date.now());
-   next();
-});
+app.set('view engine', 'pug');
+app.set('views', './views');
 
-
-app.get('/things', function(req, res){
-   res.send("Things called");
-   next();
-});
-
-app.get('*', function(req, res){
-   res.send("Page not found.");
-});
+app.get('/first_template', function(req, res){
+   res.render('first_view');
+})
 
 app.listen(3000);
